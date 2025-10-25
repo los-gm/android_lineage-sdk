a8cd1f338d5e7aaa9c23dd07464af5bf372a6678
Gustavo Mendes <gusttavo.me@outlook.com>
2025-10-25 12:51:34 -0300

    lineage-sdk: Drop force long screen
    Change-Id: I89710762a2b09ecb9d6700ccb89263e8245b8d90
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -2059,15 +2059,6 @@ public final class LineageSettings {
                 };
 
         /**
-         * List of long-screen apps.
-         */
-        public static final String LONG_SCREEN_APPS = "long_screen_apps";
-
-        /** @hide */
-        public static final Validator LONG_SCREEN_APPS_VALIDATOR =
-                sAlwaysTrueValidator;
-
-        /**
          * Force show navigation bar setting.
          * @hide
          */
@@ -2264,8 +2255,6 @@ public final class LineageSettings {
                     TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK_VALIDATOR);
             VALIDATORS.put(DISPLAY_PICTURE_ADJUSTMENT,
                     DISPLAY_PICTURE_ADJUSTMENT_VALIDATOR);
-            VALIDATORS.put(LONG_SCREEN_APPS,
-                    LONG_SCREEN_APPS_VALIDATOR);
             VALIDATORS.put(FORCE_SHOW_NAVBAR,
                     FORCE_SHOW_NAVBAR_VALIDATOR);
             VALIDATORS.put(CLICK_PARTIAL_SCREENSHOT,
